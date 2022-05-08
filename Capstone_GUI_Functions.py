e7db6280addef22be01a86d13dff46d77c8f430d
BlueSkySM <samuel.meyer@mail.wlc.edu>
2022-05-08 16:18:59 -0500

    Making Actual Progress

--- Capstone_GUI_Functions.py
+++ Capstone_GUI_Functions.py
@@ -12,31 +12,66 @@ from tkinter import filedialog as fd, END
 from collections import deque
 
 global file
+global sfile
 file = ""
+sfile = ""
 
-#imports file and updates text field with lines
 def importfile():
+    #imports file and updates text field with lines
+    #adds non-function lines to output loop
+    #when it reaches a function creates dictionary
+    #repeats till end of file
     global file
+    global sfile
+    #test hardcode
     #filer = open("dummycode.py")
     file = fd.askopenfilename()
     with open(file, 'r') as f:
         lines = f.readlines()
-    print(lines)
-    #file = filer.read()
+    #test print
+    #print(lines)
+
     
-    dfile = dictfile(lines)
-    for value in dfile.values():
-        Capstone_GUI_Elements.functiondisplay.insert(END, value + "\n")
-    #Capstone_GUI_Elements.functiondisplay.itemconfig(Capstone_GUI_Elements.parsefile, text = dfile.values())
+    dfile = dequefile(lines)
+    
+    while dfile:
+        #pop leftmost (top of program file)
+        #check for function definition
+        #if yes create deque out of function
+        #otherwise add to end of saved file
+        line = dfile.popleft()
+        #test print
+        print(line)
+        
+        if line.startswith("def"):
+            fsubdeque = deque(line)
+            while not "return" in line:
+                line = dfile.popleft()
+                fsubdeque.append(line)
+            
+            print(fsubdeque)
+            dictfunction(fsubdeque)
+        else:
+            #Capstone_GUI_Elements.functiondisplay.insert(END, line)
+            sfile += line
+    
+    #save file
+    
     return
 
-#turns file into dictionary of each line
+
 def dequefile(lines):
-    dfile = {}
-    for line_number, line in enumerate(lines):
-        dfile[line_number] = line
+    #turns file into deque so it can be popped off as it iterates
+    dfile = deque(lines)
+    #test print
     print(dfile)
     return dfile
+
+def dictfunction(subdeque):
+    #creates a dictionary of function contents from subdeque
+    #allows manipulating until next function button is pressed
+    Capstone_GUI_Elements.functiondisplay.insert(END, subdeque)
+    return
 
 
 def savefile(dfile):
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Meyer_Capstone_Project
 
-WIP
+Samuel Meyer
+
+Senior Capstone Project 2022
+
+Instructions: 
+
+This program assumes you have the Tkinter module installed, which should be installed by default. 
+
+Otherwise, open a Python program file, find your desired function, and append your comments where desired. 
+
+
